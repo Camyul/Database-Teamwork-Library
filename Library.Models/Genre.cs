a5bf891e820e3e2fbdaeeb1d52cd25d3594085c5
nefto <nefto_2000@abv.bg>
2017-06-03 10:05:31 +0300

    Add project files.

---
+++ Library.Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Library.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+        public virtual Genre ParentGenres { get; set; }
+    }
+}
---
+++ LibraryApp/LibraryDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryApp
+{
+    using Library.Models;
+    using System.Data.Entity;
+
+    class LibraryDbContext:DbContext
+    {
+        public LibraryDbContext():base("LibraryConnection")
+        {
+
+        }
+        public IDbSet<Book> Books { get; set; }
+        public IDbSet<Author> Authors { get; set; }
+        public IDbSet<Genre> Genres { get; set; }
+    }
+}
